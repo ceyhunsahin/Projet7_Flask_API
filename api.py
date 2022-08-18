250a8e7d6169e01a51c2f86531a446a1987b7d9a
ceyhunsahin <csahin17072010@gmail.com>
2022-08-18 17:01:29 +0200

    favicon cleaned, add explication

--- api.py
+++ api.py
@@ -13,14 +13,12 @@ model.load_model("pipeline_housing.json")
 # Load data test sample
 path = 'Projet_File/test_sample_data_home_risk.csv'
 path2 = 'Projet_File/test_sample_data_home_risk_normalise.csv'
-
 df_test = pd.read_csv(path)
 df_test = df_test.sample(1200, random_state=42)
-
 df_test = df_test.loc[:, ~df_test.columns.str.match ('Unnamed')]
+# Load data test sample normalisée
 df_test_normalize = pd.read_csv(path2, index_col=0)
 df_test_normalize = df_test_normalize.sample(1200, random_state=42)
-# df_test_normalize['SK_ID_CURR'] = df_test['SK_ID_CURR']
 df_test = df_test.sort_index ()
 
 # Define the threshold of for application
@@ -40,7 +38,7 @@ def home():
 # defining page for the results of a prediction via index
 @app.route ('/scores', methods=['GET'])
 def predict():
-    # get the index from a request
+    # get the index from a request, defined a data_index parameter as default
 
     if type (flask.request.args.get ('index')) is None:
         data_index = '100111'
@@ -51,31 +49,37 @@ def predict():
     df_client = df_test_normalize[df_test_normalize.index == int (data_index)]
     data = df_client.to_json ()
 
+    # predict_proba returns a list as [0,1], 0 -> for payments accepted, 1 -> for payments refused
+    # we have chosen second parameter for refused value
     score = model.predict_proba (df_client)[:, 1]
 
     df_normalize = df_test_normalize.copy ()
+    # for add probabilities, used normalized dataset
     df_proba = pd.DataFrame (model.predict_proba (df_normalize)[:, 1], columns=['proba'],
                              index=df_normalize.index.tolist ())
-
+    # for add prediction, used threshold value
     df_proba['Predict'] = np.where (df_proba['proba'] < threshold, 0, 1)
 
     df_normalize['Proba_Score'] = df_proba['proba']
     df_normalize['Predict'] = df_proba['Predict']
 
-    df_test_new_normalize = df_normalize.to_json ()
-
     #  JSON format!
-
+    df_test_new_normalize = df_normalize.to_json ()
     dict_result = { 'Credit_score': score[0], "json_data": data, 'Total_score': df_test_new_normalize }
 
+    # for json format some values are categorical, however it is difficult to handle these values as float,
+    # these values types are changed by using JSON encoder
     class NumpyFloatValuesEncoder (json.JSONEncoder):
         def default(self, obj):
             if isinstance (obj, np.float32):
                 return float (obj)
             return json.JSONEncoder.default (self, obj)
 
+    # JSON format dumps method for send the data to Dashboard
     dict_result = json.dumps (dict_result, cls=NumpyFloatValuesEncoder)
 
+    # Each request of dashboard, df_normalize dataframe adding ['Proba_Score', 'Predict'] columns,
+    # so It needs to drop these columns at the end of the API
     df_normalize.drop (['Proba_Score', 'Predict'], axis=1)
 
     return dict_result
